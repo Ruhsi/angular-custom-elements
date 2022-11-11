7f1c0e5a9622e71350dfbcb643dddbcc16e05bfc
Christoph Ruhsam <christoph.ruhsam@gepardec.com>
2022-11-11 13:59:20 +0100

    add further elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
-import {Injector, NgModule} from '@angular/core';
+import {Injector, NgModule, Type} from '@angular/core';
 import {createCustomElement} from '@angular/elements';
 import {AppComponent} from './app.component';
 import {CounterComponent} from './counter/counter.component';
 import {BrowserModule} from '@angular/platform-browser';
+import {FrontendFrameworksComponent} from './frontend-frameworks/frontend-frameworks.component';
+import {FrontendFrameworkComponent} from './frontend-frameworks/frontend-framework/frontend-framework.component';
+import {CustomElementsBase} from './base/CustomElementsBase';
+import { DisplayCenteredComponent } from './display-centered/display-centered.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    FrontendFrameworksComponent,
+    FrontendFrameworkComponent,
+    DisplayCenteredComponent
   ],
   imports: [
     BrowserModule
@@ -15,13 +22,20 @@ import {BrowserModule} from '@angular/platform-browser';
   providers: []
 })
 export class AppModule {
+
   constructor(readonly injector: Injector) {
   }
 
   ngDoBootstrap() {
-    const element = createCustomElement(CounterComponent, {
+    this.createAndDefineCustomElement('app-counter', CounterComponent);
+    this.createAndDefineCustomElement('app-frontend-frameworks', FrontendFrameworksComponent);
+    this.createAndDefineCustomElement('app-display-centered', DisplayCenteredComponent);
+  }
+
+  createAndDefineCustomElement(selector: string, component: Type<CustomElementsBase>) {
+    const element = createCustomElement<CustomElementsBase>(component, {
       injector: this.injector
-    });
-    customElements.define('counter-element', element);
+    })
+    customElements.define(selector, element);
   }
 }
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,13 +1,12 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {CustomElementsBase} from '../base/CustomElementsBase';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.scss']
 })
-export class CounterComponent {
-
-  wordsCount = 0;
+export class CounterComponent implements CustomElementsBase {
 
   @Input()
   message: string = '';
@@ -15,11 +14,10 @@ export class CounterComponent {
   @Output()
   action = new EventEmitter();
 
+  wordsCount = 0;
+
   handleClick() {
     this.wordsCount = this.message.split(' ').length;
     this.action.emit(this.wordsCount);
-  }
-
-  constructor() {
   }
 }
