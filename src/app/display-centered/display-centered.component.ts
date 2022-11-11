ab2a87794ed16a3af6ecaca3b202a2c465ee3f27
Christoph Ruhsam <christoph.ruhsam@gepardec.com>
2022-11-11 14:01:01 +0100

    add further elements

--- src/app/display-centered/display-centered.component.ts
+++ src/app/display-centered/display-centered.component.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
+import {CustomElementsBase} from '../base/CustomElementsBase';
 
 @Component({
   selector: 'app-display-centered',
   templateUrl: './display-centered.component.html',
   styleUrls: ['./display-centered.component.scss']
 })
-export class DisplayCenteredComponent {
+export class DisplayCenteredComponent implements CustomElementsBase {
 }
